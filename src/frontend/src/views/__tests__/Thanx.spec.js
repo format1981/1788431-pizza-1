aedbbe40c8259d719d652d65a06c8f31129cae10
Volkov Sergey <sergeyv@hflabs.ru>
2021-12-16 11:43:21 +0300

    fix tests

--- src/frontend/src/views/__tests__/Thanx.spec.js
+++ src/frontend/src/views/__tests__/Thanx.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import { generateMockStore } from "@/store/mocks";
 import Thanx from "@/views/Thanx.vue";
+import { authenticateUser } from "@/common/testHelpers";
 
 describe("Thanx.vue", () => {
   let actions;
@@ -38,10 +39,17 @@ describe("Thanx.vue", () => {
     expect(actions.App.hideThanx).toHaveBeenCalled();
   });
 
-  it("should route to orders page on close button click", async () => {
+  it("should route to orders page on close button click (user is authenticated)", async () => {
+    authenticateUser(store);
     createComponent({ store, mocks });
     await wrapper.find('[data-test="thanx-close"]').trigger("click");
     expect(mocks.$router.push).toHaveBeenCalledWith({ name: "Orders" });
+  });
+
+  it("should route to builder page on close button click (user is guest)", async () => {
+    createComponent({ store, mocks });
+    await wrapper.find('[data-test="thanx-close"]').trigger("click");
+    expect(mocks.$router.push).toHaveBeenCalledWith({ name: "Builder" });
   });
 
   it("should call hideThanx on ok button push", async () => {
